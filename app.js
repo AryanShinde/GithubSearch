d362d9a4fd616791be2d8afe49911cafdb45690a
AryanShinde <aryan.shinde.29@gmail.com>
2021-08-16 20:44:44 +0530

    DONEE

--- app.js
+++ app.js
@@ -15,14 +15,28 @@ form.addEventListener("submit",(e)=>{
 async function fetchApi(api){
     const response=await fetch(api);
     const jsonResponse= await response.json();
-    console.log(jsonResponse);
+    return jsonResponse;
 }
 
-function createCard(value){
+async function createCard(value){
     const userApi=`https://api.github.com/users/${value}`;
-    fetchApi(userApi);
+    const data= await fetchApi(userApi);
+    console.log(data);
     const card=document.createElement("div");
     card.classList.add("card");
-    card.innerHTML=``;
+    card.innerHTML=`
+    <div class="upper-card">
+        <img class="avatar-img" src=${data.avatar_url} alt="">
+        <h3 class="username">${data.name}</h3>
+        <a target="_blank" href=${data.html_url}>Users Github</a>
+    </div>
+    <div class="lower-card">
+        <div class="bio"><b>Bio</b>: ${data.bio}</div>
+        <div class="location"><b>Location</b>: ${data.location}</div>
+        <div class="followers"><b>Followers</b>: ${data.followers}</div>
+        <div class="following"><b>Following</b>: ${data.following}</div>
+</div>`;
+    MainDivCard.appendChild(card);
+
 }
-createCard("AryanShinde");
+createCard(inputBox.value);
